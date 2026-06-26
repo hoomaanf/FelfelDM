a3902d51574d9ebc9091a0ef56caebe8911e3faf
hoomaanf <hoomaanfelfeli@gmail.com>
2026-06-26 17:53:14 +0330

    change

--- core/local_server.py
+++ core/local_server.py
@@ -53,8 +53,8 @@ class Handler(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.end_headers()
     
     def do_GET(self):
