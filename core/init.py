0aabe7fbb44c7974a164c8c7f30b90e6105ca443
agolzarniya4213-star <a.golzarniya4213@gmail.com>
2026-06-29 02:22:15 +0330

    Update init.py

--- core/init.py
+++ core/init.py
@@ -1,25 +1,33 @@
-"""Core package for FelfelDM backend."""
+# core/__init__.py
+"""
+Core module exports for FelfelDM.
+"""
 
+from core.aria2_manager import Aria2Manager, CertificateManager
 from core.aria2_rpc import Aria2RPC
 from core.data_store import DataStore, Queue
-from core.aria2_manager import Aria2Manager
-from core.websocket_client import WebSocketClient
-from core.session_manager import SessionManager
-from core.monitor import Aria2Monitor
-from core.updater import Updater
 from core.error_handler import ErrorHandler
+from core.local_server import LocalServer
+from core.monitor import Aria2Monitor
+from core.session_manager import SessionManager
 from core.ssl_utils import create_ssl_context, get_fingerprint_from_cert
+from core.updater import Updater
+from core.websocket_client import WebSocketClient
+from core.worker import BackendWorker
 
 __all__ = [
+    "Aria2Manager",
+    "CertificateManager",
     "Aria2RPC",
     "DataStore",
     "Queue",
-    "Aria2Manager",
-    "WebSocketClient",
-    "SessionManager",
-    "Aria2Monitor",
-    "Updater",
     "ErrorHandler",
+    "LocalServer",
+    "Aria2Monitor",
+    "SessionManager",
     "create_ssl_context",
     "get_fingerprint_from_cert",
+    "Updater",
+    "WebSocketClient",
+    "BackendWorker",
 ]
