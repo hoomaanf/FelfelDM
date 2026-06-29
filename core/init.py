595a8ef53a5782bb2b43700437f57345a8d1a026
agolzarniya4213-star <a.golzarniya4213@gmail.com>
2026-06-29 12:58:47 +0330

    Update init.py

--- core/init.py
+++ core/init.py
@@ -5,10 +5,15 @@ Core module exports for FelfelDM.
 
 from core.aria2_manager import Aria2Manager, CertificateManager
 from core.aria2_rpc import Aria2RPC
-from core.data_store import DataStore, Queue
+from core.aria2_rpc_async import AsyncAria2RPC
+from core.async_worker import AsyncWorker
+from core.data_store import DataStore, Settings
 from core.error_handler import ErrorHandler
+from core.history import HistoryManager
 from core.local_server import LocalServer
 from core.monitor import Aria2Monitor
+from core.queue_model import Queue
+from core.service_container import ServiceContainer, create_container, get_container, reset_container
 from core.session_manager import SessionManager
 from core.ssl_utils import create_ssl_context, get_fingerprint_from_cert
 from core.updater import Updater
@@ -17,17 +22,25 @@ from core.worker import BackendWorker
 
 __all__ = [
     "Aria2Manager",
-    "CertificateManager",
-    "Aria2RPC",
-    "DataStore",
-    "Queue",
-    "ErrorHandler",
-    "LocalServer",
     "Aria2Monitor",
+    "Aria2RPC",
+    "AsyncAria2RPC",
+    "AsyncWorker",
+    "BackendWorker",
+    "CertificateManager",
+    "DataStore",
+    "ErrorHandler",
+    "HistoryManager",
+    "LocalServer",
+    "Queue",
+    "ServiceContainer",
     "SessionManager",
-    "create_ssl_context",
-    "get_fingerprint_from_cert",
+    "Settings",
     "Updater",
     "WebSocketClient",
-    "BackendWorker",
+    "create_container",
+    "create_ssl_context",
+    "get_container",
+    "get_fingerprint_from_cert",
+    "reset_container",
 ]
