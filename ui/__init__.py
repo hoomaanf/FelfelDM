1db6a04bd3e16716439d3fd53995d9967462c945
agolzarniya4213-star <a.golzarniya4213@gmail.com>
2026-06-29 22:05:29 +0330

    Create __init__.py

---
+++ ui/__init__.py
@@ -0,0 +1,15 @@
+# =============================================================================
+# ui/__init__.py
+# =============================================================================
+"""UI package containing all user interface components."""
+
+__all__ = [
+    "main_window",
+    "dialogs",
+    "table_model",
+    "delegates",
+    "animated_dialog",
+    "icons",
+    "controllers",
+    "style",
+]
