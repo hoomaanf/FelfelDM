a2b9206aefdfbf495da6eadd79350524ea5a9463
HOOMAAN.F <hoomaanfelfeli@gmail.com>
2026-07-02 19:59:38 +0330

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -8,7 +8,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 echo -e "${BLUE}╔══════════════════════════════════════════════════════════════╗${NC}"
-echo -e "${BLUE}║     🗑️  FelfelDM - Uninstaller                            ║${NC}"
+echo -e "${BLUE}║     🗑️  FelfelDM - Uninstaller                               ║${NC}"
 echo -e "${BLUE}╚══════════════════════════════════════════════════════════════╝${NC}"
 echo ""
 
