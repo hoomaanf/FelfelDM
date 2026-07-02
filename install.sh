404fadbafccc046c2f5c91e88968720f66b9cb37
HOOMAAN.F <hoomaanfelfeli@gmail.com>
2026-07-02 19:58:44 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 echo -e "${BLUE}╔══════════════════════════════════════════════════════════════╗${NC}"
-echo -e "${BLUE}║     🔨  FelfelDM - System Package Installer               ║${NC}"
+echo -e "${BLUE}║     🔨  FelfelDM - System Package Installer                  ║${NC}"
 echo -e "${BLUE}╚══════════════════════════════════════════════════════════════╝${NC}"
 echo ""
 
